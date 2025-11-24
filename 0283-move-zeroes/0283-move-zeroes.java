2ff6bd37e654cab1874024f2ff4432748be28e30
Hiroshiniprojects <projectsofhiro@gmail.com>
2025-11-24 20:51:30 +0530

    Time: 5 ms (9.98%), Space: 47.7 MB (16.95%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+class Solution {
+    public void moveZeroes(int[] nums) {
+        List<Integer> list=new ArrayList<>();
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                list.add(nums[i]);
+            }
+        }
+        for(int i=0;i<list.size();i++){
+            nums[i]=list.get(i);
+        }
+        int size=list.size();
+
+        for(int i=size;i<nums.length;i++){
+            nums[i]=0;
+        }
+
+
+    }
+}
