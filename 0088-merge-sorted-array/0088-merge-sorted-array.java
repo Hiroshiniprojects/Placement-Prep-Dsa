446d28e13e179c82aa136dfdf1e1cb1f73e35801
Hiroshiniprojects <projectsofhiro@gmail.com>
2026-03-22 17:59:47 +0530

    Time: 0 ms (100%), Space: 44.1 MB (22.27%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,17 +1,25 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        mergeRec(nums1, nums2, m - 1, n - 1, m + n - 1);
-    }
-
-    private void mergeRec(int[] nums1, int[] nums2, int i, int j, int k) {
-        if (j < 0) return; // nums2 fully merged
-
-        if (i >= 0 && nums1[i] > nums2[j]) {
-            nums1[k] = nums1[i];
-            mergeRec(nums1, nums2, i - 1, j, k - 1);
-        } else {
-            nums1[k] = nums2[j];
-            mergeRec(nums1, nums2, i, j - 1, k - 1);
+        int[]merged=new int[m+n];
+        int left=0;
+        int right=0;
+        int index=0;
+        while(left<m && right<n){
+            if(nums1[left]<=nums2[right]){
+                merged[index++]=nums1[left++];
+            }
+            else{
+                merged[index++]=nums2[right++];
+            }
+        }
+        while(left<m){
+            merged[index++]=nums1[left++];
+        }
+        while(right<n){
+            merged[index++]=nums2[right++];
+        }
+        for(int i=0;i<m+n;i++){
+            nums1[i]=merged[i];
         }
     }
 }
