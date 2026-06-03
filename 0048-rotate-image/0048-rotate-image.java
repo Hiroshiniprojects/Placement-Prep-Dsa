3c5ec2e5285cf8b9e79f62381dcb1cc2d4aa2373
Hiroshiniprojects <projectsofhiro@gmail.com>
2026-06-03 22:26:35 +0530

    Time: 0 ms (100%), Space: 43.6 MB (67.83%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<m;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+            for(int k=0;k<n;k++){
+                for(int l=0;l<m/2;l++){
+                    int temp=matrix[k][l];
+                    matrix[k][l]=matrix[k][n-1-l];
+                    matrix[k][n-1-l]=temp;
+                }
+            }
+
+        
+        
+    }
+}
